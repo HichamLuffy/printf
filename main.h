d7a7614da978b6567c63b5766e2e9d8c1bb02bcd
hicham <hicham@gmail.com>
2023-04-25 04:38:50 -0700

    task 3

--- main.h
+++ main.h
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <strings.h>
-
+int _print_unsigned(const char *format, ...);
 void print_num(int num);
 int handle_string(char *str);
 int handle_number(int num);
---
+++ print_unsigned.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _printf - prints
+ * @format: string to be printed
+ * @...: args
+ * Return: length of format
+ */
+
+int _print_unsigned(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+    while (*format) {
+        if (*format == '%') {
+            format++;
+            if (*format == 'u') {
+                unsigned int num = va_arg(args, unsigned int);
+                count += printf("%u", num);
+            } else if (*format == 'o') {
+                unsigned int num = va_arg(args, unsigned int);
+                count += printf("%o", num);
+            } else if (*format == 'x') {
+                unsigned int num = va_arg(args, unsigned int);
+                count += printf("%x", num);
+            } else if (*format == 'X') {
+                unsigned int num = va_arg(args, unsigned int);
+                count += printf("%X", num);
+            } else {
+                putchar(*format);
+                count++;
+            }
+        } else {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
