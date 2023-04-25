6dd852323177ad7d2267aa82a65569a56b302e2f
hicham <hicham@gmail.com>
2023-04-25 04:43:05 -0700

    task 3

--- print_unsigned.c
+++ print_unsigned.c
@@ -13,31 +13,37 @@ int _print_unsigned(const char *format, ...)
 
     va_start(args, format);
     while (*format) {
-        if (*format == '%') {
+        if (*format == '%')
+	{
             format++;
-            if (*format == 'u') {
-                unsigned int num = va_arg(args, unsigned int);
-                count += printf("%u", num);
-            } else if (*format == 'o') {
-                unsigned int num = va_arg(args, unsigned int);
-                count += printf("%o", num);
-            } else if (*format == 'x') {
-                unsigned int num = va_arg(args, unsigned int);
-                count += printf("%x", num);
-            } else if (*format == 'X') {
-                unsigned int num = va_arg(args, unsigned int);
-                count += printf("%X", num);
-            } else {
-                putchar(*format);
-                count++;
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-        format++;
+            if (*format == 'u')
+	    {
+		    unsigned int num = va_arg(args, unsigned int);
+    		    count += printf("%u", num);   
+	    } else if (*format == 'o')
+	    {
+		    unsigned int num = va_arg(args, unsigned int);
+		    count += printf("%o", num);
+	    } else if (*format == 'x')
+	    {
+		    unsigned int num = va_arg(args, unsigned int);
+		    count += printf("%x", num);
+	    } else if (*format == 'X')
+	    {
+		    unsigned int num = va_arg(args, unsigned int);
+		    count += printf("%X", num);
+	    } else
+	    {
+		    putchar(*format);
+		    count++;
+	    }
+	} else
+	{
+		putchar(*format);
+		count++;
+	}
+	format++;
     }
-
     va_end(args);
-    return count;
+    return (count);
 }
