6847aa3c3e1511bb38c9296d208117c1573fd082
Loubna <loubna.alfidi@gmail.com>
2023-04-25 03:45:26 -0700

    update

--- handl.c
+++ handl.c
@@ -9,7 +9,7 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 /**
- * num - prints
+ * len_num - prints
  * @num: string to be printed
  * Return: length of num
  */
@@ -20,54 +20,54 @@ int len_num(int num)
 	return (1 + len_num(num / 10));
 }
 
-/*
- * handle_number - chekc
+/**
+ * handle_number - check code
  * @num: parametre
- * Return - 1
+ * Return: 1
  */
 int handle_number(int num)
 {
-    if (num == -2147483648)
-    {
-        _putchar('-');
-        _putchar('2');
-        handle_number(147483648);
-        return (1);
-    }
-    else if (num < 0)
-    {
-        _putchar('-');
-        num = -num;
-    }
-    if (num >= 10)
-    {
-        handle_number(num / 10);
-        handle_number(num % 10);
-    }
-    else if (num < 10)
-    {
-        _putchar(num + '0');
-    }
-    return (1);
+	if (num == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		handle_number(147483648);
+		return (1);
+	}
+	else if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if (num >= 10)
+	{
+		handle_number(num / 10);
+		handle_number(num % 10);
+	}
+	else if (num < 10)
+	{
+		_putchar(num + '0');
+	}
+	return (1);
 }
-/*
- * handle_string - check
+/**
+ * handle_string - check code
  * @str: parametre
- * Return - i
+ * Return: i
  */
 int handle_string(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    if (str == NULL)
-    {
-        handle_string("(null)");
-        return (6);
-    }
-    while (str[i])
-    {
-        _putchar(str[i]);
-        i++;
-    }
-    return (i);
+	if (str == NULL)
+	{
+		handle_string("(null)");
+		return (6);
+	}
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
 }
