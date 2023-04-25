6c4b78201f2d937a90a027cfb8dd29058ff41c12
hicham <hicham@gmail.com>
2023-04-25 16:58:12 -0700

    task 0

--- printf.c
+++ printf.c
@@ -7,12 +7,12 @@
  */
 void print_num(int num)
 {
-        int count = 0;
+	int count = 0;
 
-        if (num < 0)
-                count++;
-        count += len_num(num);
-        handle_number(num);
+	if (num < 0)
+		count++;
+	count += len_num(num);
+	handle_number(num);
 }
 /**
  * _printf - prints
@@ -22,50 +22,50 @@ void print_num(int num)
  */
 int _printf(const char *format, ...)
 {
-        char c, *str;
-        int num, count = 0;
-        va_list args;
+	char c, *str;
+	int num, count = 0;
+	va_list args;
 
-        va_start(args, format);
-        if (!format || !format[0])
-                return (-1);
-        while (*format)
-        {
-                if (*format == '%')
-                {
-                        format++;
-                        if (*format == '\0')
-                                return (-1);
-                        if (*format == 'c')
-                        {
-                                c = va_arg(args, int);
-                                count += _putchar(c);
-                        } else if (*format == 's')
-                        {
-                                str = va_arg(args, char *);
-                                count += handle_string(str);
-                        } else if (*format == 'd' || *format == 'i')
-                        {
-                                num = va_arg(args, int);
-                                if (num < 0)
-                                        count++;
-                                count += len_num(num);
-                                handle_number(num);
-                        } else if (*format == '%')
-                                count += _putchar('%');
-                        else 
-                        {
-                                count += _putchar('%');
-                                if (*format)
-                                        count += _putchar(*format);
-                        }
-                        format++;
-                } else 
-                {
-                        count += _putchar(*format);
-                        format++;
-                }
-        }
-        va_end(args);
-        return (count);
+	va_start(args, format);
+	if (!format || !format[0])
+		return (-1);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				return (-1);
+			if (*format == 'c')
+			{
+				c = va_arg(args, int);
+				count += _putchar(c);
+			} else if (*format == 's')
+			{
+				str = va_arg(args, char *);
+				count += handle_string(str);
+			} else if (*format == 'd' || *format == 'i')
+			{
+				num = va_arg(args, int);
+				if (num < 0)
+					count++;
+				count += len_num(num);
+				handle_number(num);
+			} else if (*format == '%')
+				count += _putchar('%');
+			else 
+			{
+				count += _putchar('%');
+				if (*format)
+					count += _putchar(*format);
+			}
+			format++;
+		} else 
+		{
+			count += _putchar(*format);
+			format++;
+		}
+	}
+	va_end(args);
+	return (count);
 }
