d738f2256aba8e429b7ab89860d7fb04536d4663
Loubna <loubna.alfidi@gmail.com>
2023-04-25 06:59:25 -0700

    update

--- main.c
+++ main.c
@@ -5,16 +5,10 @@
  *
  * Return: Always 0
  */
-int main()
+int main(void)
 {
-<<<<<<< HEAD
 	unsigned int num = 42;
 
 	print_binary("Binary representation of 42 is %b.\n", num);
 	return (0);
-=======
-  _printf("hello %d %i", 423, 12);
-
-  return (0);
->>>>>>> 2a6c888a29ce1e20a92130b2e0a6000ad7e894ae
 }
