8481b9918488c0ec848522c8b411ed492de995ed
hicham <hicham@gmail.com>
2023-04-25 20:00:05 -0700

    task

--- main.c
+++ main.c
@@ -1,66 +1,60 @@
 #include "main.h"
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
 int main(void)
 {
-	int a = 0, b = 0;
+    int a = 0, b = 0;
 
-	/* ========================= */
-	/* 			%d CASES		 */
-	/* ========================= */
-	printf("======================\n");
-	printf("****** d CASES ******\n");
-	printf("======================\n");
-	a = printf("Expected output:    %d\n", 1134001);
-	b = printf_id("Current output:     %d\n", 1134001);
-	printf("Expected length:    %d\n", a);
-	printf("Current length:     %d\n", b);
-	a = printf("Expected output:    %d\n", -4567);
-	b = printf_id("Current output:     %d\n", -4567);
-	printf("Expected length:    %d\n", a);
-	printf("Current length:     %d\n", b);
-	a = printf("Expected output:    %d\n", 0);
-	b = printf_id("Current output:     %d\n", 0);
-	printf("Expected length:    %d\n", a);
-	printf("Current length:     %d\n", b);
-	a = printf("Expected output:    %d\n", INT_MAX);
-	b = printf_id("Current output:     %d\n", INT_MAX);
-	printf("Expected length:    %d\n", a);
-	printf("Current length:     %d\n", b);
-	a = printf("Expected output:    %d\n", INT_MIN);
-	b = printf_id("Current output:     %d\n", INT_MIN);
-	printf("Expected length:    %d\n", a);
-	printf("Current length:     %d\n", b);
+    /* ========================= */
+    /*             %d CASES         */
+    /* ========================= */
+    printf("======================\n");
+    printf("****** d CASES ******\n");
+    printf("======================\n");
+    a = printf("Expected output:    %d\n", 1134001);
+    b = _printf("Current output:     %d\n", 1134001);
+    printf("Expected length:    %d\n", a);
+    printf("Current length:     %d\n", b);
+    a = printf("Expected output:    %d\n", -4567);
+    b = _printf("Current output:     %d\n", -4567);
+    printf("Expected length:    %d\n", a);
+    printf("Current length:     %d\n", b);
+    a = printf("Expected output:    %d\n", 0);
+    b = _printf("Current output:     %d\n", 0);
+    printf("Expected length:    %d\n", a);
+    printf("Current length:     %d\n", b);
+    a = printf("Expected output:    %d\n", INT_MAX);
+    b = _printf("Current output:     %d\n", INT_MAX);
+    printf("Expected length:    %d\n", a);
+    printf("Current length:     %d\n", b);
+    a = printf("Expected output:    %d\n", INT_MIN);
+    b = _printf("Current output:     %d\n", INT_MIN);
+    printf("Expected length:    %d\n", a);
+    printf("Current length:     %d\n", b);
 
-	/* ========================= */
-	/* 			%i CASES		 */
-	/* ========================= */
-	printf("======================\n");
-	printf("****** i CASES ******\n");
-	printf("======================\n");
-	a = printf("Expected output:    %i\n", 237482);
-	b = printf_id("Current output:     %i\n", 237482);
-	printf("Expected length:    %i\n", a);
-	printf("Current length:     %i\n", b);
-	a = printf("Expected output:    %i\n", -328472);
-	b = printf_id("Current output:     %i\n", -328472);
-	printf("Expected length:    %i\n", a);
-	printf("Current length:     %i\n", b);
-	a = printf("Expected output:    %i\n", 0);
-	b = printf_id("Current output:     %i\n", 0);
-	printf("Expected length:    %i\n", a);
-	printf("Current length:     %i\n", b);
-	a = printf("Expected output:    %i\n", INT_MAX);
-	b = printf_id("Current output:     %i\n", INT_MAX);
-	printf("Expected length:    %i\n", a);
-	printf("Current length:     %i\n", b);
-	a = printf("Expected output:    %i\n", INT_MIN);
-	b = printf_id("Current output:     %i\n", INT_MIN);
-	printf("Expected length:    %i\n", a);
-	printf("Current length:     %i\n", b);
-	return (0);
+    /* ========================= */
+    /*             %i CASES         */
+    /* ========================= */
+    printf("======================\n");
+    printf("****** i CASES ******\n");
+    printf("======================\n");
+    a = printf("Expected output:    %i\n", 237482);
+    b = _printf("Current output:     %i\n", 237482);
+    printf("Expected length:    %i\n", a);
+    printf("Current length:     %i\n", b);
+    a = printf("Expected output:    %i\n", -328472);
+    b = _printf("Current output:     %i\n", -328472);
+    printf("Expected length:    %i\n", a);
+    printf("Current length:     %i\n", b);
+    a = printf("Expected output:    %i\n", 0);
+    b = _printf("Current output:     %i\n", 0);
+    printf("Expected length:    %i\n", a);
+    printf("Current length:     %i\n", b);
+    a = printf("Expected output:    %i\n", INT_MAX);
+    b = _printf("Current output:     %i\n", INT_MAX);
+    printf("Expected length:    %i\n", a);
+    printf("Current length:     %i\n", b);
+    a = printf("Expected output:    %i\n", INT_MIN);
+    b = _printf("Current output:     %i\n", INT_MIN);
+    printf("Expected length:    %i\n", a);
+    printf("Current length:     %i\n", b);
+    return (0);
 }
--- printf.c
+++ printf.c
@@ -1,64 +1,90 @@
 #include "main.h"
+/**
+ * _putchar - check
+ * @c: parametre
+ * Return: length of format
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
 
 /**
- * print_num - prints num
- * @num: int
- * Return: void
+ * num - prints
+ * @num: string to be printed
+ * Return: length of num
  */
-void print_num(int num)
+int len_num(int num)
 {
-	int count = 0;
-
-	if (num < 0)
-		count++;
-	count += len_num(num);
-	handle_number(num);
+    if (num == 0)
+        return (0);
+    return (1 + len_num(num / 10));
 }
 /**
  * _printf - prints
- * @format: const char *
- * @...
- * Return: int
+ * @format: string to be printed
+ * @...: args
+ * Return: length of format
  */
 int _printf(const char *format, ...)
 {
-	char c, *str;
-	int count = 0;
-	va_list args;
+    int count = 0;
+    va_list args;
 
-	va_start(args, format);
-	if (!format || !format[0])
-		return (-1);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == '\0')
-				return (-1);
-			if (*format == 'c')
-			{
-				c = va_arg(args, int);
-				count += _putchar(c);
-			} else if (*format == 's')
-			{
-				str = va_arg(args, char *);
-				count += handle_string(str);
-			} else if (*format == '%')
-				count += _putchar('%');
-			else
-			{
-				count += _putchar('%');
-				if (*format)
-					count += _putchar(*format);
-			}
-			format++;
-		} else
-		{
-			count += _putchar(*format);
-			format++;
-		}
-	}
-	va_end(args);
-	return (count);
+    va_start(args, format);
+    if (!format || !format[0])
+        return (-1);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c') 
+            {
+                char c = va_arg(args, int);
+
+                count += _putchar(c);
+
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+
+                count += handle_string(str);
+            }
+            else if (*format == '%')
+            {
+                _putchar('%');
+                count++;
+            }
+            else if (*format == 'd' || *format == 'i')
+            {
+                int num = va_arg(args, int);
+
+                if (num < 0)
+                    count++;
+                count += len_num(num);
+                handle_number(num);
+            }
+            else if (*format == '\0')
+                return (-1);
+            else{
+                _putchar('%');
+                count++;
+                if (*format)    
+                {
+                    _putchar(*format);   
+                    count++;    
+                }
+            }
+            format++;
+        }
+        else
+        {
+            _putchar(*format);
+            format++;
+            count++;
+        }
+    }
+    return (count);
 }
