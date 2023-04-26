f34a6f143b1e7e72b5ae44d386c289507eec0bff
Loubna <loubna.alfidi@gmail.com>
2023-04-26 10:53:45 -0700

    upd

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <strings.h>
 #include <limits.h>
-int id_printf(const char *format, ...);
+
 int _print_unsigned(const char *format, ...);
 void print_num(int num);
 int handle_string(char *str);
@@ -18,7 +18,5 @@ void print_string(char *str);
 int _putchar(char c);
 int len_num(int num);
 int _printf(const char *format, ...);
-int print_binary(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
 
 #endif
