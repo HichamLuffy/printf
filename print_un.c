4fe6146b68d5e581b0158b3e766e4bd9542603c2
hicham <hicham@gmail.com>
2023-04-26 13:35:19 -0700

    test

--- print_un.c
+++ print_un.c
@@ -6,36 +6,45 @@
  * Return - 1
  */
 
-void custom_printf(const char* fmt, ...) {
+void custom_printf(const char* fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
     int i = 0;
     while (fmt[i] != '\0') {
         if (fmt[i] == '%') {
-            if (fmt[i+1] == 'b') {
+            if (fmt[i+1] == 'b')
+	    {
                 unsigned int arg = va_arg(args, unsigned int);
                 unsigned int mask = 1U << ((sizeof(unsigned int) * 8) - 1);
                 int digits = 0;
-                while (mask > 0) {
-                    if (arg & mask) {
+
+                while (mask > 0)
+		{
+                    if (arg & mask)
+		    {
                         putchar('1');
                         digits++;
-                    } else if (digits > 0 || mask == 1) {
+                    } else if (digits > 0 || mask == 1)
+		    {
                         putchar('0');
                         digits++;
                     }
                     mask >>= 1;
                 }
-                if (digits == 0) {
+                if (digits == 0)
+		{
                     putchar('0');
                 }
                 i += 2;
-            } else {
+            } else
+	    {
                 putchar(fmt[i]);
                 putchar(fmt[i+1]);
                 i += 2;
             }
-        } else {
+        } else
+	{
             putchar(fmt[i]);
             i++;
         }
