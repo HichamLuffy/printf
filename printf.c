414542668c94e7ecddf20940f4b1339fee3d3746
Loubna <loubna.alfidi@gmail.com>
2023-04-26 10:15:51 -0700

    update

--- printf.c
+++ printf.c
@@ -7,63 +7,51 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
+	int count = 0;
+	va_list args;
 
-    va_start(args, format);
-    if (!format || !format[0])
-        return (-1);
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c') 
-            {
-                char c = va_arg(args, int);
+	va_start(args, format);
+	if (!format || !format[0])
+		return (-1);
+	while (*format++)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				return (-1);
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
 
-                count += _putchar(c);
+				count += _putchar(c);
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
 
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
+				count += handle_string(str);
+			}
+			else if (*format == '%')
+				count += _putchar('%');
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(args, int);
 
-                count += handle_string(str);
-            }
-            else if (*format == '%')
-            {
-                _putchar('%');
-                count++;
-            }
-            else if (*format == 'd' || *format == 'i')
-            {
-                int num = va_arg(args, int);
-
-                if (num < 0)
-                    count++;
-                count += len_num(num);
-                handle_number(num);
-            }
-            else if (*format == '\0')
-                return (-1);
-            else{
-                _putchar('%');
-                count++;
-                if (*format)    
-                {
-                    _putchar(*format);   
-                    count++;    
-                }
-            }
-            format++;
-        }
-        else
-        {
-            _putchar(*format);
-            format++;
-            count++;
-        }
-    }
-    return (count);
+				if (num < 0)
+					count++;
+				count += len_num(num);
+				handle_number(num);
+			} else
+			{
+				count += _putchar('%');
+				if (*format)
+				{
+					count += _putchar(*format);
+				}
+			}
+		} else
+			count += _putchar(*format);
+	}
+	return (count);
 }
