b2ee7c81441bee3c1113fbb15e416c352c2b8ac7
Loubna <loubna.alfidi@gmail.com>
2023-04-25 19:56:55 -0700

    test

--- main.c
+++ main.c
@@ -1,65 +1,63 @@
 #include "main.h"
-
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: int
  */
+
 int main(void)
 {
 	int a = 0, b = 0;
 
 	/* ========================= */
-	/* 			%d CASES		 */
+	/*             %d CASES         */
 	/* ========================= */
 	printf("======================\n");
 	printf("****** d CASES ******\n");
 	printf("======================\n");
 	a = printf("Expected output:    %d\n", 1134001);
-	b = id_printf("Current output:     %d\n", 1134001);
+	b = _printf("Current output:     %d\n", 1134001);
 	printf("Expected length:    %d\n", a);
 	printf("Current length:     %d\n", b);
-	a = printf("Expected output:    %d\n", -4567);
-	b = id_printf("Current output:     %d\n", -4567);
+	a = printf("Expected output:    %d\n", -4567);	b = _printf("Current output:     %d\n", -4567);
 	printf("Expected length:    %d\n", a);
 	printf("Current length:     %d\n", b);
 	a = printf("Expected output:    %d\n", 0);
-	b = id_printf("Current output:     %d\n", 0);
+	b = _printf("Current output:     %d\n", 0);
 	printf("Expected length:    %d\n", a);
 	printf("Current length:     %d\n", b);
 	a = printf("Expected output:    %d\n", INT_MAX);
-	b = id_printf("Current output:     %d\n", INT_MAX);
+	b = _printf("Current output:     %d\n", INT_MAX);
 	printf("Expected length:    %d\n", a);
 	printf("Current length:     %d\n", b);
 	a = printf("Expected output:    %d\n", INT_MIN);
-	b = id_printf("Current output:     %d\n", INT_MIN);
+	b = _printf("Current output:     %d\n", INT_MIN);
 	printf("Expected length:    %d\n", a);
 	printf("Current length:     %d\n", b);
-
 	/* ========================= */
-	/* 			%i CASES		 */
+	/*             %i CASES         */
 	/* ========================= */
 	printf("======================\n");
 	printf("****** i CASES ******\n");
 	printf("======================\n");
 	a = printf("Expected output:    %i\n", 237482);
-	b = id_printf("Current output:     %i\n", 237482);
+	b = _printf("Current output:     %i\n", 237482);
 	printf("Expected length:    %i\n", a);
 	printf("Current length:     %i\n", b);
 	a = printf("Expected output:    %i\n", -328472);
-	b = id_printf("Current output:     %i\n", -328472);
+	b = _printf("Current output:     %i\n", -328472);
 	printf("Expected length:    %i\n", a);
 	printf("Current length:     %i\n", b);
 	a = printf("Expected output:    %i\n", 0);
-	b = id_printf("Current output:     %i\n", 0);
+	b = _printf("Current output:     %i\n", 0);
 	printf("Expected length:    %i\n", a);
 	printf("Current length:     %i\n", b);
 	a = printf("Expected output:    %i\n", INT_MAX);
-	b = id_printf("Current output:     %i\n", INT_MAX);
+	b = _printf("Current output:     %i\n", INT_MAX);
 	printf("Expected length:    %i\n", a);
 	printf("Current length:     %i\n", b);
 	a = printf("Expected output:    %i\n", INT_MIN);
-	b = id_printf("Current output:     %i\n", INT_MIN);
+	b = _printf("Current output:     %i\n", INT_MIN);
 	printf("Expected length:    %i\n", a);
 	printf("Current length:     %i\n", b);
 	return (0);
