d5728bb37a873d87cc1abe9b6571488a7b72dbfc
Loubna <loubna.alfidi@gmail.com>
2023-04-27 19:55:04 -0700

    test

---
+++ _printfn.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _printfn - prints
+ * @format: string to be printed
+ * @...: args
+ * Return: length of format
+ */
+int _printfn(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				return (-1);
+			if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(args, int);
+
+				handle_number(num);
+				count += len_num(num);
+			} else
+			{
+				_putchar(*format);
+				count++;
+				format++;
+			}
+			format++;
+		} else
+		{
+			count += _putchar(*format);
+			format++;
+		}
+	}
+	va_end(args);
+	return (count);
+}
